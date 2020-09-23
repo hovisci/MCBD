2a279a7771fb66623cfa956a8d84fbf65166a59c
usehovisci <ciarahovis@gmail.com>
2020-09-23 16:05:09 -0400

    Added script to update all cleaned surveys at once

---
+++ scripts/UpdateData.R
@@ -0,0 +1,23 @@
+# Code to update all data files
+rm(list=ls())               
+
+# Raw data 
+knitr::purl(input = "./scripts/RawSurveyPrep.Rmd", output = "./scripts/RawSurveyPrep.R" )
+source('./scripts/RawSurveyPrep.R')
+
+# Survey 1 data clean
+knitr::purl(input = "./scripts/Survey1_cleanup.Rmd", output = "./scripts/Survey1_cleanup.R" )
+source('./scripts/Survey1_cleanup.R')
+
+# Survey 2 data clean
+knitr::purl(input = "./scripts/Survey2_cleanup.Rmd", output = "./scripts/Survey2_cleanup.R" )
+source('./scripts/Survey2_cleanup.R')
+
+# Survey 3 data clean
+knitr::purl(input = "./scripts/Survey3_cleanup.Rmd", output = "./scripts/Survey3_cleanup.R" )
+
+source('./scripts/Survey3_cleanup.R')
+
+# Survey 2 & 3 merge
+knitr::purl(input = "./scripts/S23_merge.Rmd", output = "./scripts/S23_merge.R" )
+source('./scripts/S23_merge.R')
